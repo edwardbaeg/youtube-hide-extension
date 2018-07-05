2b5a4ceb5eef58d559f4aab4d7d5dfc9e52d9430
edwardbaeg <edwardbaeg9@gmail.com>
2018-07-04 21:46:35 -0700

    Greys out low rated video titles on page load

--- src/main.js
+++ src/main.js
@@ -1,25 +1,54 @@
 const YT_VIDEO_URL = 'https://www.googleapis.com/youtube/v3/videos';
 
 $(document).ready(function() {
-  console.log('Hi YouTube!');
+  console.log('YOUTUBE HIDE extension start!');
 
-  // results page
+  // TODO: check current URL for 'results?search'
+  // TODO: use regex after `=` and before optional `&`
+  // on the results page, get video titles' href prop
   $("[id=video-title]").each(function() {
     // console.log(this);
+    let videoId = $(this).attr('href');
+    videoId = videoId.slice(9, 20);
+    fetchStats(videoId, (videos, context) => {
+      printRating(videos, context);
+    }, this);
+
   });
-  fetchStats('1GrNv3ow9H8',printRatings);
+
+  /**
+   * Can also get all the stats with just one ajax requst
+   * Submit videoids as a string separated by commas
+   */
+  // let videoIds = [];
+  // videoIds.push(videoId);
+  // videoIdString = videoIds.join(',');
+  // fetchStats(videoIdString, printRatings);
+
 });
 
+// TODO: move functions to a different file?
+// this context is the video title
 const printRatings = (videos) => {
   for (let vid of videos) {
     let {likeCount, dislikeCount} = vid.statistics;
-    console.log('likeCount', likeCount);
-    console.log('dislikeCount', dislikeCount);
+    console.log('likeCount', likeCount, 'dislikeCount', dislikeCount);
     console.log('percentage: ', +likeCount / (+likeCount + +dislikeCount));
   }
 };
 
-const fetchStats = function(videoId, cb) {
+const printRating = (vid, context) => {
+  let {likeCount, dislikeCount} = vid[0].statistics;
+  let percentage = +likeCount / (+likeCount + +dislikeCount) * 100;
+  console.log(context);
+  console.log($(context).attr('title'));
+  console.log('likeCount', likeCount, 'dislikeCount', dislikeCount, 'percentage', percentage);
+  if (percentage < 80) {
+    $(context).css('color', 'lightgrey');
+  }
+};
+
+const fetchStats = function(videoId, cb, context) {
   $.get({
     url: YT_VIDEO_URL,
     data: {
@@ -28,8 +57,8 @@ const fetchStats = function(videoId, cb) {
       key: window.YT_API_KEY
     },
     success(results) {
-      console.log('success!', results);
-      cb(results.items);
+      // console.log('success!', results);
+      cb(results.items, context);
     },
     error() {
       console.error('error');
